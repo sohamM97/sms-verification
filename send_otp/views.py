f686d85e69d8de6a148f6bfa694f3e9410b6fa16
soham1705 <soham.m15@iiits.in>
2017-08-27 16:00:21 +0530

    Send OTP to mobile no. accepted from user

--- send_otp/views.py
+++ send_otp/views.py
@@ -13,23 +13,24 @@ class MyForm(forms.Form):
 	
 	
 
-#def generate_otp():
-#	otp=random.randint(1000,9999)
-#	print otp
-#	account_sid = "AC07ab8e0938b8758dd2cc29336754c258"
-#	auth_token = "e9a340fa42a1a11f2279e23e830221e8"
-#	client = Client(account_sid, auth_token)
-#	client.messages.create(
-#		to="+917095972533",
-#		from_="+18635765103",
-#		body='Your One Time Password is '+str(otp))
+def generate_otp(mobile_no):
+	otp=random.randint(1000,9999)
+	print otp
+	account_sid = "AC07ab8e0938b8758dd2cc29336754c258"
+	auth_token = "e9a340fa42a1a11f2279e23e830221e8"
+	client = Client(account_sid, auth_token)
+	client.messages.create(
+		to=mobile_no,
+		from_="+18635765103",
+		body='Your One Time Password is '+str(otp))
 
 def send_otp(request):
-	#generate_otp()
+
 	form=MyForm(request.POST)
 	if form.is_valid():
 		cd=form.cleaned_data
 		mobile_no=cd.get('mobile_no')
 		print mobile_no
+		generate_otp(mobile_no)
 	return render(request,'send_otp\index.html',{'form':form})
 
